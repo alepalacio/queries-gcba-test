9aac1cfc5697ba78ca63df7e987f7a2e6c7ff890
Jose Alejandro Palacio <76705732+alepalacio@users.noreply.github.com>
2022-09-27 17:17:57 -0300

    first commit

---
+++ script.sql
@@ -0,0 +1,28 @@
+select count(1) from cur3d_parcela 
+
+select count(distinct geom) from cur3d_parcela
+
+select count(distinct smp) from cur3d_parcela
+
+select count(1) from mindesarrollourbanoytransporte_codigourbanistico 
+
+select count(distinct the_geom) from mindesarrollourbanoytransporte_codigourbanistico
+
+select count(distinct smp1) from mindesarrollourbanoytransporte_codigourbanistico
+
+select count(1) 
+from mindesarrollourbanoytransporte_codigourbanistico cod, cur3d_parcela par
+where cod.smp1 = par.smp 
+
+select smp, count(smp) from cur3d_parcela
+group by smp
+having count(1) > 1
+
+union
+select smp1, count(smp1) from mindesarrollourbanoytransporte_codigourbanistico
+group by smp1 
+having count(1) > 1
+
+select geom from cur3d_parcela where smp = '023-162-000'
+
+select geom from cur3d_parcela where smp = '021-019-014a'
